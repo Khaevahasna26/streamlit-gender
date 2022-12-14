76af19bb725ef66027bef2b33accbecbc1018321
Khaevahasna26 <120515877+Khaevahasna26@users.noreply.github.com>
2022-12-15 00:03:44 +0700

    Update stream-gender.py

--- stream-gender.py
+++ stream-gender.py
@@ -2,7 +2,7 @@ import pickle
 import streamlit as st
 
 # load save model 
-model = pickle.load(open('gender_Model.sav', 'rb'))
+model = pickle.load(open('gender_model.sav', 'rb'))
 
 # Judul Untuk Web
 st.title('Data Mining Prediksi Jenis Kelamin')
